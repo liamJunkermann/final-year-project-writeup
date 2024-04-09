afc19f9bfb649bc7e5b84f23db7578c9af863f3b
Liam Junkermann <dev@ljsoft.xyz>
2024-04-09 12:15:14 +0100

    adding intro to word count

--- wc.sh
+++ wc.sh
@@ -1,1 +1,1 @@
-perl texcount.pl background/background.tex project/data-collection.tex project/data-analysis-viz.tex project/machine-learning.tex discussion/discussion.tex conclusion/conclusion.tex
+perl texcount.pl introduction/introduction.tex background/background.tex project/data-collection.tex project/data-analysis-viz.tex project/machine-learning.tex discussion/discussion.tex conclusion/conclusion.tex
